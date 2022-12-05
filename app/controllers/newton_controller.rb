41bfc85a343f9822e716a76cb99bc8f7e6e189c0
TNTTroll <MyBlocks@mail.ru>
2022-12-06 00:37:41 +0300

    First attemp

---
+++ app/controllers/newton_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Class Newton
+class NewtonController < ApplicationController
+  protect_from_forgery with: :null_session,
+                       if: Proc.new { |c| c.request.format =~ %r{application/json} }
+
+  def result
+    render xml: find_sqrt
+  end
+
+  def find_sqrt
+    a = params[:a].to_i
+    
+    x = (a + 1.0) / 2.0
+    loop do
+      break if (x*x - a) / a < 0.001
+      x = (x + a/x) / 2.0
+    end
+    [a, x]
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root 'newton#result'
+
+end
